4d09f70177197a1e28812d297636601f3928781f
Kumar Thangavel <kumar.thangavel1@justice.gov.uk>
2025-11-24 14:43:36 +0000

    Ipb 1848/adding post for creating referrals (#3)
    * Api for creating the referral in the db

--- src/main/kotlin/uk/gov/justice/digital/hmpps/communitysupportapi/service/ReferralService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/communitysupportapi/service/ReferralService.kt
@@ -3,6 +3,7 @@ package uk.gov.justice.digital.hmpps.communitysupportapi.service
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
+import uk.gov.justice.digital.hmpps.communitysupportapi.dto.CreateReferralRequest
 import uk.gov.justice.digital.hmpps.communitysupportapi.entity.Referral
 import uk.gov.justice.digital.hmpps.communitysupportapi.repository.ReferralRepository
 import java.util.Optional
@@ -18,4 +19,18 @@ class ReferralService(
   }
 
   fun getReferral(referralId: UUID): Optional<Referral> = referralRepository.findById(referralId)
+
+  fun createReferral(createReferralRequest: CreateReferralRequest): Referral {
+    val referral = Referral(
+      id = UUID.randomUUID(),
+      firstName = createReferralRequest.firstName,
+      lastName = createReferralRequest.lastName,
+      crn = createReferralRequest.crn,
+      referenceNumber = createReferralRequest.referenceNumber,
+      sex = createReferralRequest.sex,
+      dateOfBirth = createReferralRequest.dateOfBirth,
+      ethnicity = createReferralRequest.ethnicity,
+    )
+    return referralRepository.save(referral)
+  }
 }
