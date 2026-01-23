3f28a4d10e78aef6da24878163fb1ec25b409a7a
Davansah <david.ansah@digital.justice.gov.uk>
2026-01-23 14:35:56 +0000

    IPB-1887/Web Client Configuration Update (#44)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/communitysupportapi/config/WebClientConfiguration.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/communitysupportapi/config/WebClientConfiguration.kt
@@ -1,9 +1,12 @@
 package uk.gov.justice.digital.hmpps.communitysupportapi.config
 
+import org.springframework.beans.factory.ObjectProvider
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
 import org.springframework.security.oauth2.client.AuthorizedClientServiceReactiveOAuth2AuthorizedClientManager
 import org.springframework.security.oauth2.client.ReactiveOAuth2AuthorizedClientManager
 import org.springframework.security.oauth2.client.ReactiveOAuth2AuthorizedClientService
@@ -14,12 +17,12 @@ import uk.gov.justice.hmpps.kotlin.auth.reactiveAuthorisedWebClient
 import java.time.Duration
 
 @Configuration
+@Profile("!test")
 class WebClientConfiguration(
   @Value("\${hmpps-auth.url}") private val hmppsAuthBaseUri: String,
   @Value("\${api.health-timeout:2s}") private val healthTimeout: Duration,
   @Value("\${services.ndelius-integration-api.base-url}") private val deliusBaseUrl: String,
   @Value("\${services.nomis-api.base-url}") private val nomisBaseUrl: String,
-  private val authorizedClientManager: ReactiveOAuth2AuthorizedClientManager,
 ) {
   companion object {
     const val COMMUNITY_SUPPORT_API_CLIENT_ID = "community-support-api-client"
@@ -31,7 +34,7 @@ class WebClientConfiguration(
   fun hmppsAuthHealthWebClient(builder: WebClient.Builder): WebClient = builder.healthWebClient(hmppsAuthBaseUri, healthTimeout)
 
   @Bean
-  @ConditionalOnMissingBean
+  @ConditionalOnBean(ReactiveClientRegistrationRepository::class)
   fun reactiveOAuth2AuthorizedClientManager(
     clientRegistrations: ReactiveClientRegistrationRepository,
     authorizedClientService: ReactiveOAuth2AuthorizedClientService,
@@ -41,18 +44,20 @@ class WebClientConfiguration(
   )
 
   @Bean("deliusWebClient")
-  @ConditionalOnMissingBean(name = ["deliusWebClient"])
-  fun deliusWebClient(builder: WebClient.Builder): WebClient = builder.reactiveAuthorisedWebClient(
-    authorizedClientManager,
-    COMMUNITY_SUPPORT_API_CLIENT_ID,
-    deliusBaseUrl,
-  )
+  fun deliusWebClient(builder: WebClient.Builder, authorizedClientManager: ObjectProvider<ReactiveOAuth2AuthorizedClientManager>): WebClient = authorizedClientManager.ifAvailable?.let { manager ->
+    builder.reactiveAuthorisedWebClient(
+      manager,
+      COMMUNITY_SUPPORT_API_CLIENT_ID,
+      deliusBaseUrl,
+    )
+  } ?: builder.baseUrl(deliusBaseUrl).build()
 
   @Bean("nomisWebClient")
-  @ConditionalOnMissingBean(name = ["nomisWebClient"])
-  fun nomisWebClient(builder: WebClient.Builder): WebClient = builder.reactiveAuthorisedWebClient(
-    authorizedClientManager,
-    COMMUNITY_SUPPORT_API_CLIENT_ID,
-    nomisBaseUrl,
-  )
+  fun nomisWebClient(builder: WebClient.Builder, authorizedClientManager: ObjectProvider<ReactiveOAuth2AuthorizedClientManager>): WebClient = authorizedClientManager.ifAvailable?.let { manager ->
+    builder.reactiveAuthorisedWebClient(
+      manager,
+      COMMUNITY_SUPPORT_API_CLIENT_ID,
+      nomisBaseUrl,
+    )
+  } ?: builder.baseUrl(nomisBaseUrl).build()
 }
