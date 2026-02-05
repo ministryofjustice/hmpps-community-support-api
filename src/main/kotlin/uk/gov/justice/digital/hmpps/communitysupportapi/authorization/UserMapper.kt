3dd680a3c09eb7d743dcdaa039dd4b70968ea7bf
Kumar Thangavel <kumar.thangavel1@justice.gov.uk>
2026-02-05 12:20:12 +0000

    Ipb 2014/case list view 3 (#52)
    * fixing the auth source issues

--- src/main/kotlin/uk/gov/justice/digital/hmpps/communitysupportapi/authorization/UserMapper.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/communitysupportapi/authorization/UserMapper.kt
@@ -25,7 +25,7 @@ class UserMapper(
         null
       }
 
-    val userId = userName?.let { manageUsersClient.getUserDetails(it)?.userId }
+    val userDetails = userName?.let { manageUsersClient.getUserDetails(it) }
       ?: run {
         if (userName != null) {
           errors.add("no 'user_id' claim in token")
@@ -34,7 +34,9 @@ class UserMapper(
       }
 
     val authSource = authenticationHolder.authentication.authSource
-    if (authSource == AuthSource.NONE) {
+      .let { userDetails?.authSource ?: it.name }
+
+    if (authSource == AuthSource.NONE.name) {
       errors.add("no 'auth_source' claim in token")
     }
 
@@ -42,13 +44,13 @@ class UserMapper(
       throwAccessDenied(errors)
     }
 
-    return referralUserRepository.findByHmppsAuthId(requireNotNull(userId))
+    return referralUserRepository.findByHmppsAuthId((userDetails?.userId!!))
       ?: referralUserRepository.save(
         ReferralUser(
           id = UUID.randomUUID(),
-          hmppsAuthId = userId,
-          authSource = authSource.name,
-          hmppsAuthUsername = requireNotNull(userName),
+          hmppsAuthId = userDetails.userId,
+          authSource = authSource,
+          hmppsAuthUsername = userName!!,
         ),
       )
   }
