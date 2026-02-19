1386742219276b5167d501f6dd9ce16f5c5e23f4
Kumar Thangavel <kumar.thangavel1@justice.gov.uk>
2026-02-19 09:59:27 +0000

    fixing schema spy report (#60)

--- src/test/kotlin/uk/gov/justice/digital/hmpps/communitysupportapi/InitialiseDatabase.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/communitysupportapi/InitialiseDatabase.kt
@@ -3,10 +3,22 @@ package uk.gov.justice.digital.hmpps.communitysupportapi
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT
+import org.springframework.context.annotation.Import
 import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.context.TestPropertySource
+import uk.gov.justice.digital.hmpps.communitysupportapi.integration.TestWebClientConfiguration
 
 @SpringBootTest(webEnvironment = RANDOM_PORT)
 @ActiveProfiles("test")
+@Import(TestWebClientConfiguration::class)
+@TestPropertySource(
+  properties = [
+    "services.hmpps-auth-api.base-url=http://localhost:8090/auth",
+    "services.ndelius-integration-api.base-url=http://localhost:9999",
+    "services.nomis-api.base-url=http://localhost:9999",
+    "services.manage-users-api.base-url=http://localhost:9999",
+  ],
+)
 class InitialiseDatabase {
 
   // This is needed to initialize the database for schema spy
