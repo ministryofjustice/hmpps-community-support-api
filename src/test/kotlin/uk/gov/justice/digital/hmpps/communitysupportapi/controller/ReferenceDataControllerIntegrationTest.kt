f3914f6326f5e3065def09ee9d764ef19d800dee
moj-keith-au <Keith.Au@justice.gov.uk>
2026-02-25 14:32:03 +0000

    Add controller to surface probation office data to frontend

---
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/communitysupportapi/controller/ReferenceDataControllerIntegrationTest.kt
@@ -0,0 +1,104 @@
+package uk.gov.justice.digital.hmpps.communitysupportapi.controller
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+import org.springframework.core.ParameterizedTypeReference
+import uk.gov.justice.digital.hmpps.communitysupportapi.integration.IntegrationTestBase
+import uk.gov.justice.digital.hmpps.communitysupportapi.model.ProbationOffice
+
+class ReferenceDataControllerIntegrationTest : IntegrationTestBase() {
+
+  @Nested
+  @DisplayName("GET /bff/reference-data/probation-offices")
+  inner class ReferenceDataEndpoint {
+
+    @Test
+    fun `should return unauthorized if no token`() {
+      webTestClient.get()
+        .uri("/bff/reference-data/probation-offices")
+        .exchange()
+        .expectStatus()
+        .isUnauthorized
+    }
+
+    @Test
+    fun `should return forbidden if no role`() {
+      webTestClient.get()
+        .uri("/bff/reference-data/probation-offices")
+        .headers(
+          setAuthorisation(
+            "AUTH_ADM",
+            listOf(),
+            listOf("read"),
+          ),
+        )
+        .exchange()
+        .expectStatus()
+        .isForbidden
+    }
+
+    @Test
+    fun `should return forbidden if wrong role`() {
+      webTestClient.get()
+        .uri("/bff/reference-data/probation-offices")
+        .headers(setAuthorisation(roles = listOf("ROLE_WRONG")))
+        .exchange()
+        .expectStatus()
+        .isForbidden
+    }
+
+    @Test
+    fun `should return list of probation offices information`() {
+      val response = webTestClient.get()
+        .uri { uriBuilder ->
+          uriBuilder
+            .path("/bff/reference-data/probation-offices")
+            .queryParam("refresh", "false")
+            .build()
+        }
+        .headers(setAuthorisation())
+        .exchange()
+        .expectStatus().isOk
+        .expectBody(object : ParameterizedTypeReference<List<ProbationOffice>>() {})
+        .returnResult().responseBody!!
+
+      assertThat(response).hasSize(130)
+      response.forEach { probationOffice ->
+        assertThat(probationOffice.probationOfficeId).isNotNull()
+        assertThat(probationOffice.name).isNotBlank()
+        assertThat(probationOffice.address).isNotBlank()
+        assertThat(probationOffice.probationRegionId).isNotBlank()
+      }
+    }
+  }
+
+  private fun createProbationOffices(): List<ProbationOffice> {
+    val probationOffices: List<ProbationOffice> = listOf(
+      ProbationOffice(
+        1,
+        "Derby: Derwent Centre",
+        "Derwent Centre, 1 Stuart Street, Derby, DE1 2EQ",
+        "F",
+        "https://www.gov.uk/guidance/derby-derwent-centre",
+      ),
+      ProbationOffice(
+        5,
+        "Leicestershire: Coalville Probation Office",
+        "Probation Office, 27 London Road, Coalville, Leicestershire, LE67 3JB",
+        "F",
+        "https://www.gov.uk/guidance/leicestershire-coalville-probation-office",
+        "CRS0086",
+      ),
+      ProbationOffice(
+        probationOfficeId = 128,
+        name = "Warrington: Warrington Probation Office",
+        address = "Units 3 & 4 Bankside, Crosfield Street, Warrington, WA1 1UP",
+        probationRegionId = "B",
+        deliusCRSLocationId = "CRS0328",
+      ),
+    )
+    return probationOffices
+  }
+}
