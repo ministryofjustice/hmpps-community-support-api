cb260a0f61ce57122616eefc5dc2013b2ab6631b
Keith Au <Keith.Au@justice.gov.uk>
2026-05-20 15:30:15 +0100

    IPB-2256/Change caseworker assignment using caseRefId (#127)
    * Change referral user assignment functions from using referral id to case reference number
    
    * - make exception handling consistent
    - remove duplicated codes
    - remove unnecessary error message returned
    
    ---------
    
    Co-authored-by: wilson <58164272+thomaswilsonxyz@users.noreply.github.com>

--- src/main/kotlin/uk/gov/justice/digital/hmpps/communitysupportapi/repository/ReferralRepository.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/communitysupportapi/repository/ReferralRepository.kt
@@ -1,10 +1,14 @@
 package uk.gov.justice.digital.hmpps.communitysupportapi.repository
 
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 import uk.gov.justice.digital.hmpps.communitysupportapi.entity.Referral
 import java.util.UUID
 
 interface ReferralRepository : JpaRepository<Referral, UUID> {
   fun existsByReferenceNumber(reference: String): Boolean
   fun findByReferenceNumber(referenceNumber: String): MutableList<Referral>
+
+  @Query("SELECT r FROM Referral r WHERE r.referenceNumber = :referenceNumber")
+  fun findReferenceNumberOrNull(referenceNumber: String): Referral?
 }
--- src/main/kotlin/uk/gov/justice/digital/hmpps/communitysupportapi/service/ReferralAssignmentService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/communitysupportapi/service/ReferralAssignmentService.kt
@@ -2,20 +2,23 @@ package uk.gov.justice.digital.hmpps.communitysupportapi.service
 
 import jakarta.persistence.EntityManager
 import org.slf4j.LoggerFactory
-import org.springframework.http.HttpStatus
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
-import org.springframework.web.server.ResponseStatusException
 import uk.gov.justice.digital.hmpps.communitysupportapi.dto.AssignmentFailureDto
 import uk.gov.justice.digital.hmpps.communitysupportapi.dto.CaseWorkerDto
 import uk.gov.justice.digital.hmpps.communitysupportapi.dto.UserDto
+import uk.gov.justice.digital.hmpps.communitysupportapi.entity.Referral
 import uk.gov.justice.digital.hmpps.communitysupportapi.entity.ReferralUser
 import uk.gov.justice.digital.hmpps.communitysupportapi.entity.ReferralUserAssignment
 import uk.gov.justice.digital.hmpps.communitysupportapi.entity.UserType
+import uk.gov.justice.digital.hmpps.communitysupportapi.exception.NotFoundException
 import uk.gov.justice.digital.hmpps.communitysupportapi.mapper.toEntity
 import uk.gov.justice.digital.hmpps.communitysupportapi.model.AssignCaseWorkersResult
+import uk.gov.justice.digital.hmpps.communitysupportapi.model.CaseIdentifier
 import uk.gov.justice.digital.hmpps.communitysupportapi.repository.ReferralRepository
 import uk.gov.justice.digital.hmpps.communitysupportapi.repository.ReferralUserAssignmentRepository
+import uk.gov.justice.digital.hmpps.communitysupportapi.validation.CaseIdentifierValidator
 import uk.gov.justice.hmpps.kotlin.auth.AuthSource
 import java.time.LocalDateTime
 import java.util.UUID
@@ -31,15 +34,22 @@ class ReferralAssignmentService(
   companion object {
     private const val MAX_CASE_WORKERS = 5
     private val emailPattern = Pattern.compile("^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$")
+    private val identifierValidator: CaseIdentifierValidator = CaseIdentifierValidator()
     private val log = LoggerFactory.getLogger(this::class.java)
   }
 
-  fun getAssignedCaseWorkers(referralId: UUID): List<CaseWorkerDto>? {
-    log.info("Get assigned case workers of a referral {}", referralId)
+  fun getReferralByCaseIdentifier(caseIdentifier: String?): Referral = when (val identifier = ReferralAssignmentService.Companion.identifierValidator.validate(caseIdentifier)) {
+    is CaseIdentifier.ReferralId -> referralRepository.findByIdOrNull(identifier.value)
+      ?: throw NotFoundException("Referral not found for id ${identifier.value}")
 
-    val referral = referralRepository.findById(referralId)
-      .orElseThrow { ResponseStatusException(HttpStatus.NOT_FOUND, "Referral not found") }
+    is CaseIdentifier.CaseId -> referralRepository.findReferenceNumberOrNull(identifier.value)
+      ?: throw NotFoundException("Referral not found for reference ${identifier.value}")
+  }
 
+  fun getAssignedCaseWorkers(identifier: String): List<CaseWorkerDto>? {
+    log.info("Get assigned case workers of a referral {}", identifier)
+
+    val referral = getReferralByCaseIdentifier(identifier)
     val activeAssignments: List<ReferralUserAssignment> = referralUserAssignmentRepository.findAllByReferralIdAndNotDeleted(referral.id)
 
     return activeAssignments
@@ -54,11 +64,10 @@ class ReferralAssignmentService(
   }
 
   @Transactional
-  fun assignCaseWorkers(assigner: ReferralUser, referralId: UUID, caseWorkers: List<CaseWorkerDto>): AssignCaseWorkersResult? {
-    log.info("Assigning case workers to referral {}", referralId)
+  fun assignCaseWorkers(assigner: ReferralUser, identifier: String, caseWorkers: List<CaseWorkerDto>): AssignCaseWorkersResult? {
+    log.info("Assigning case workers to referral {}", identifier)
 
-    val referral = referralRepository.findById(referralId)
-      .orElseThrow { ResponseStatusException(HttpStatus.NOT_FOUND, "Referral not found") }
+    val referral = getReferralByCaseIdentifier(identifier)
 
     inputValidation(caseWorkers)?.let { return it }
 
@@ -123,7 +132,7 @@ class ReferralAssignmentService(
         },
         succeededList = submittedAssignments.map { CaseWorkerDto(userType = if (it.second.authSource == AuthSource.AUTH.source) UserType.INTERNAL else UserType.EXTERNAL, userId = it.second.id, it.second.fullName, it.second.hmppsAuthUsername) },
       )
-      log.info("Caseworkers assigned to referral {}: {}", referralId, result)
+      log.info("Caseworkers assigned to referral {}: {}", identifier, result)
       return result
     } else {
       return AssignCaseWorkersResult(
@@ -176,11 +185,6 @@ class ReferralAssignmentService(
       return AssignCaseWorkersResult(
         success = false,
         message = "Cannot assign more than $MAX_CASE_WORKERS caseworkers.",
-      )
-    } else if (uniqueEmailsCount.isEmpty()) {
-      return AssignCaseWorkersResult(
-        success = false,
-        message = "No email address provided.",
       )
     }
     return null
