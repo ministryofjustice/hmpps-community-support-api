039c50f39d4a0b26957bb1f4663ff8f032c40ba8
moj-keith-au <Keith.Au@justice.gov.uk>
2026-07-03 13:42:16 +0100

    - merge SubmitReferralRequest and ReferralAdditionalDetails into a single file

--- src/main/kotlin/uk/gov/justice/digital/hmpps/communitysupportapi/model/SubmitReferralRequest.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/communitysupportapi/model/SubmitReferralRequest.kt
@@ -1,5 +1,12 @@
 package uk.gov.justice.digital.hmpps.communitysupportapi.model
 
+import uk.gov.justice.digital.hmpps.communitysupportapi.dto.PersonAdditionalSupportNeedsDto
+
+data class ReferralAdditionalDetails(
+  val supportNeeds: PersonAdditionalSupportNeedsDto? = null,
+  // future extends structure here
+)
+
 data class SubmitReferralRequest(
   val additionalInformation: ReferralAdditionalDetails? = null,
   // future extends structure here
