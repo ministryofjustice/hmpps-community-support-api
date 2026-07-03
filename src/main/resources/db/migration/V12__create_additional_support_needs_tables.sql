1681ba32de064e4082f68688e37e49285650803f
moj-keith-au <Keith.Au@justice.gov.uk>
2026-07-03 09:47:37 +0100

    - remove unused script

--- src/main/resources/db/migration/V12__create_additional_support_needs_tables.sql
+++ src/main/resources/db/migration/V12__create_additional_support_needs_tables.sql
@@ -48,21 +48,3 @@ COMMENT ON COLUMN person_additional_support_needs.diversity_details IS 'Details 
 COMMENT ON COLUMN person_additional_support_needs.anything_else_details IS 'Free text for Anything else';
 COMMENT ON COLUMN person_additional_support_needs.no_additional_support_needed IS 'User explicitly indicated no additional support needed';
 COMMENT ON COLUMN person_additional_support_needs.interpreter_language IS 'Specific language for interpreter';
-
--- create referral and person support needs relationship table
--- CREATE TABLE IF NOT EXISTS referral_support_needs (
---     id UUID NOT NULL PRIMARY KEY,,
---     referral_id UUID NOT NULL,
---     person_support_needs_id UUID NOT NULL,
---
---     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
---     created_by UUID REFERENCES referral_user(id),
---
---     CONSTRAINT fk_rsn_referral FOREIGN KEY (referral_id) REFERENCES referral(id) ON DELETE CASCADE,
---     CONSTRAINT fk_rsn_support_needs FOREIGN KEY (person_support_needs_id) REFERENCES person_additional_support_needs(id) ON DELETE CASCADE,
---     CONSTRAINT uk_rsn_referral UNIQUE (referral_id)
--- );
---
--- -- Indexes
--- CREATE INDEX idx_rsn_referral_id ON referral_person_support_needs(referral_id);
--- CREATE INDEX idx_rsn_support_needs_id ON referral_person_support_needs(person_support_needs_id);
