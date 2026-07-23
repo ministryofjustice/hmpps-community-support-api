78bb1198e1d172981779d99f333c1bb57cabbee5
Francis O'Brien <francis.obrien@justice.gov.uk>
2026-07-23 16:17:46 +0100

    update tasklist DTO to return status text and tag

--- src/main/kotlin/uk/gov/justice/digital/hmpps/communitysupportapi/dto/TaskListStatusResponseDto.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/communitysupportapi/dto/TaskListStatusResponseDto.kt
@@ -1,10 +1,35 @@
 package uk.gov.justice.digital.hmpps.communitysupportapi.dto
 
+import uk.gov.justice.digital.hmpps.communitysupportapi.entity.Person
+import uk.gov.justice.digital.hmpps.communitysupportapi.entity.Referral
+
 data class TaskListStatusResponseDto(
   val fullName: String,
-  val confirmPersonalDetailsCompleted: Boolean,
-  val checkRiskInformationCompleted: Boolean,
-  val selectThePersonsNeedsCompleted: Boolean,
-  val addDetailsOfAnyAdditionalSupportNeedsCompleted: Boolean,
-  val addDetailsOfMainPointOfContactCompleted: Boolean,
-)
+  val confirmPersonalDetailsCompleted: TaskListStatusItem,
+  val checkRiskInformationCompleted: TaskListStatusItem,
+  val selectThePersonsNeedsCompleted: TaskListStatusItem,
+  val addDetailsOfAnyAdditionalSupportNeedsCompleted: TaskListStatusItem,
+  val addDetailsOfMainPointOfContactCompleted: TaskListStatusItem,
+) {
+  companion object {
+    fun from(person: Person, referral: Referral) = TaskListStatusResponseDto(
+      fullName = "$person.firstName $person.lastName",
+      TaskListStatusItem.notStarted(),
+      TaskListStatusItem.notStarted(),
+      TaskListStatusItem.notStarted(),
+      TaskListStatusItem.notStarted(),
+      TaskListStatusItem.notStarted(),
+    )
+  }
+}
+
+data class TaskListStatusItem(
+  val completed: Boolean,
+  val statusText: String,
+  val tag: String? = null,
+) {
+  companion object {
+    fun completed() = TaskListStatusItem(true, "Completed", "govuk-tag--grey")
+    fun notStarted() = TaskListStatusItem(false, "Not started", "govuk-tag--blue")
+  }
+}
