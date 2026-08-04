3fe0d7ca7fca0d8fad6b10b5351d9babb1ab72fb
Francis O'Brien <francis.obrien@justice.gov.uk>
2026-08-04 15:18:48 +0100

    Implement action plan persistence updates and consolidate IPB-2567 notes

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/communitysupportapi/entity/ActionPlanTemplate.kt
@@ -0,0 +1,15 @@
+package uk.gov.justice.digital.hmpps.communitysupportapi.entity
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+import java.util.UUID
+
+@Entity
+@Table(name = "action_plan_template")
+data class ActionPlanTemplate(
+  @Id
+  @Column(name = "id")
+  val id: UUID,
+)
