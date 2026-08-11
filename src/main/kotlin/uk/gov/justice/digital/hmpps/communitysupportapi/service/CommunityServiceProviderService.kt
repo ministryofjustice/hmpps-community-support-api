c80cd0c58b3188f6a64efa6ebb5df57a6ddbfeda
kumar.thangavel <kumar.thangavel1@justice.gov.uk>
2026-08-11 11:33:51 +0100

    include pdus in community service provider dto

--- src/main/kotlin/uk/gov/justice/digital/hmpps/communitysupportapi/service/CommunityServiceProviderService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/communitysupportapi/service/CommunityServiceProviderService.kt
@@ -2,15 +2,28 @@ package uk.gov.justice.digital.hmpps.communitysupportapi.service
 
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
+import uk.gov.justice.digital.hmpps.communitysupportapi.dto.CommunitySupportServiceDto
+import uk.gov.justice.digital.hmpps.communitysupportapi.dto.CommunitySupportServicesDto
 import uk.gov.justice.digital.hmpps.communitysupportapi.repository.CommunityServiceProviderRepository
+import uk.gov.justice.digital.hmpps.communitysupportapi.repository.PduRepository
 
 @Service
 class CommunityServiceProviderService(
   private val communityServiceProviderRepository: CommunityServiceProviderRepository,
+  private val pduRepository: PduRepository,
 ) {
   companion object {
     private val log = LoggerFactory.getLogger(this::class.java)
   }
 
-  fun communityServiceProviders() = communityServiceProviderRepository.findAll()
+  fun communityServiceProviders(): CommunitySupportServicesDto {
+    val providers = communityServiceProviderRepository.findAll()
+    val services = providers.map { provider ->
+      val pdus = pduRepository.findByContractAreaId(provider.contractArea.id)
+        .map { it.name }
+        .sorted()
+      CommunitySupportServiceDto.from(provider, pdus)
+    }
+    return CommunitySupportServicesDto(communitySupportServices = services)
+  }
 }
