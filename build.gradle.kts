912c9449d3c641c2e94976a792f703c42e650538
Thomas Wilson-Cook <thomas.wilson-cook@justice.gov.uk>
2026-09-23 10:59:11 +0100

    chore: Bump HMPPS Kotlin template to 11.0.9 to fix mismatched ktlint resolution (expected kotlin 2.4.10 got 2.4.20)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "11.0.8"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "11.0.9"
   kotlin("plugin.spring") version "2.4.20"
   kotlin("plugin.jpa") version "2.4.20"
 }
