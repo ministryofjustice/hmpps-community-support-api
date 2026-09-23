f29d40738ad2abb964a0b5491c9b5f5ab515ed12
Thomas Wilson-Cook <thomas.wilson-cook@justice.gov.uk>
2026-09-23 13:39:24 +0100

    chore: downgrade springdoc-openapi-starter-webmvc-ui to version 3.1.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,7 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-webmvc")
   implementation("org.springframework.boot:spring-boot-starter-security")
   implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
-  implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:3.1.1")
+  implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:3.1.0")
   implementation("org.springframework.boot:spring-boot-starter-actuator")
   implementation("org.springframework.boot:spring-boot-starter-flyway")
   implementation("org.springframework:spring-web")
