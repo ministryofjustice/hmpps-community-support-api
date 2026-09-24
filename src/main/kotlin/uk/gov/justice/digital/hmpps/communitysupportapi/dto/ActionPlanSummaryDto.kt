3ceb19d089e9ee1a946f90929b73fb8eba87ca2f
Thomas Swann <thomas.swann@justice.gov.uk>
2026-09-24 12:10:02 +0100

    IPB-2764: Update Action Plan Summary BFF (#337)
    * IPB-2764: Action Plan summary bff returns activity information on need outcomes
    
    * IPB-2764: Fix integration tests
    
    * IPB-2764: Lint
    
    ---------
    
    Co-authored-by: Kumar Thangavel <kumar.thangavel1@justice.gov.uk>

--- src/main/kotlin/uk/gov/justice/digital/hmpps/communitysupportapi/dto/ActionPlanSummaryDto.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/communitysupportapi/dto/ActionPlanSummaryDto.kt
@@ -1,5 +1,7 @@
 package uk.gov.justice.digital.hmpps.communitysupportapi.dto
 
+import uk.gov.justice.digital.hmpps.communitysupportapi.entity.ActionPlanActivity
+import uk.gov.justice.digital.hmpps.communitysupportapi.entity.ActionPlanStepQuestionAnswerDetails
 import java.util.UUID
 
 data class ActionPlanSummaryDto(
@@ -14,6 +16,34 @@ data class ActionPlanSummaryDto(
   data class ActionPlanSummaryNeed(
     val id: UUID,
     val label: String,
-    val outcomes: List<String> = emptyList(),
+    val outcomes: List<ActionPlanSummaryOutcome> = emptyList(),
   )
+
+  data class ActionPlanSummaryOutcome(
+    val id: UUID,
+    val label: String,
+    val activities: List<ActionPlanSummaryOutcomeActivity> = emptyList(),
+  ) {
+    companion object {
+      fun from(actionPlanStepQuestionAnswerDetails: ActionPlanStepQuestionAnswerDetails, actionPlanActivities: Collection<ActionPlanActivity>): ActionPlanSummaryOutcome = ActionPlanSummaryOutcome(
+        actionPlanStepQuestionAnswerDetails.actionPlanStepQuestionAnswerHeaderId,
+        label = actionPlanStepQuestionAnswerDetails.content.orEmpty(),
+        activities = actionPlanActivities.map { activity -> ActionPlanSummaryOutcomeActivity.from(activity) },
+      )
+    }
+  }
+
+  data class ActionPlanSummaryOutcomeActivity(
+    val id: UUID,
+    val who: String,
+    val details: String,
+  ) {
+    companion object {
+      fun from(actionPlanActivity: ActionPlanActivity): ActionPlanSummaryOutcomeActivity = ActionPlanSummaryOutcomeActivity(
+        actionPlanActivity.id,
+        actionPlanActivity.who,
+        details = actionPlanActivity.activityDetails,
+      )
+    }
+  }
 }
